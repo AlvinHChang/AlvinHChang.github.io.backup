a195ae7982f0365d35e80ba63756bf0aa1d51f2b
AlvinHChang <solstory@gmail.com>
2018-12-10 23:02:26 -0600

    added dropdown

---
+++ app/components/Dropdown/index.js
@@ -0,0 +1,26 @@
+/**
+ *
+ * Dropdown Component
+ *
+ */
+
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import Select from './Select';
+import ToggleOption from '../ToggleOption';
+
+class Dropdown extends React.Component {
+  render() {
+
+  }
+}
+
+Toggle.propTypes = {
+  onToggle: PropTypes.func,
+  values: PropTypes.array,
+  value: PropTypes.string,
+  messages: PropTypes.object,
+};
+
+export default Toggle;
